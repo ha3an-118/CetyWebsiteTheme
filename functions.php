a85281b8233f23693d160bcfacff31e222c60580
Abbas <abbasbabaei76@gmail.com>
2019-08-29 01:18:35 +0430

    added custom logo support and register nav menu

--- functions.php
+++ functions.php
@@ -2,6 +2,19 @@
 //Actions
 add_action('wp_enqueue_scripts', 'IncludeStylesAndScripts');
 
+add_action('after_setup_theme', 'CustomLogoSetup');
+
+
+
+
+
+//Registers
+require_once(get_template_directory() . '/inc/Helpers/View/HeaderNavMenuHelper.php');
+RegisterNavMenus();
+
+
+
+
 
 //-------Functions
 function IncludeStylesAndScripts()
@@ -13,4 +26,18 @@ function IncludeStylesAndScripts()
     $Boundle->Include('Bootstrap');
     $Boundle->Include('WebsiteStyle');
     $Boundle->Include('WebsiteScript');
+}
+
+
+function CustomLogoSetup()
+{
+    $CustomLogoInfo = array(
+        'height'      => 100,
+        'width'       => 400,
+        'flex-height' => true,
+        'flex-width'  => true,
+        'header-text' => array('site-title', 'site-description'),
+    );
+
+    add_theme_support('custom-logo', $CustomLogoInfo);
 }
