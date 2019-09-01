f945dcad8b9cbd47e01aa96e2b83d0adb28bf948
Abbas <abbasbabaei76@gmail.com>
2019-09-01 16:13:37 +0430

    update footer.php to use template parts

--- footer.php
+++ footer.php
@@ -1,19 +1,2 @@
-<!-- footer part -->
-<footer>
-    <div class="bg-white-cety d-flex align-items-center justify-content-between py-2 px-3 ">
-        <div class="d-flex align-items-center">
-            <a href=""><i class="fab fa-twitter fa-1x text-dark-purple-cety hover-text-purple-cety focus-text-purple-cety mx-1"></i></a>
-            <a href="https://www.instagram.com/cetyir/"><i class="fab fa-instagram fa-1x text-dark-purple-cety hover-text-purple-cety focus-text-purple-cety mx-1"></i></a>
-            <a href=""><i class="fa fa-paper-plane border-none fa-1x text-dark-purple-cety hover-text-purple-cety focus-text-purple-cety mx-1"></i></a>
-        </div>
-        <div class="fa-06x">تمامی حقوق مادی و معنوی این سایت متعلق به cety میباشد </div>
-    </div>
-</footer>
-<!-- end footer part -->
-
-
-<?php wp_footer(); ?>
-
-</body>
-
-</html>
+<?php
+get_template_part('template-parts/Footer/Default/Default');
