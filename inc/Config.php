21a6d7743c68bf03c9ce6f291b83b540c2771eca
Abbas <abbasbabaei76@gmail.com>
2019-09-11 02:39:14 +0430

    more configs for blog page

--- inc/Config.php
+++ inc/Config.php
@@ -4,6 +4,17 @@
  * This file contains all variables and defaults for the whole theme
  */
 
+//theme locations
 const HeaderNavigationMenuLocation = 'HeaderNavMenu';
 const HeaderSocialMediaMenuLocation = 'HeaderSocialMenu';
 const FooterSocialMediaMenuLocation = 'FooterSocialMenu';
+
+//post excerpt
+const BlogPageExcerptMaxWords = 10;
+const BlogPageExcerptMorePlaceHolder = ' ...';
+
+//pagination
+const NumberPageNumbersArroundCurrentPageNumber = 2;
+
+//blog page
+const NumberPostsPerPage = 4;
