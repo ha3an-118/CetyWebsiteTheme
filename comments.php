e63a47e536b19068caa6336c26901c0c232274f5
Abbas <abbasbabaei76@gmail.com>
2019-09-13 21:47:16 +0430

    added single.php and it's template parts

---
+++ comments.php
@@ -0,0 +1,36 @@
+<?php comment_form(); ?>
+
+<div class="space-20"></div> <!-- this is for spacing -->
+
+<hr>
+
+<div class="space-20"></div> <!-- this is for spacing -->
+
+<?php if (have_comments()) { ?>
+
+    <!-- comments list section -->
+    <section class="px-3">
+
+        <!-- title of this section -->
+        <div class="text-purple-cety text-right">
+            <span class="pl-1"><?php esc_html_e(get_comment_count()['all']); ?></span><span> <?php _e('دیدگاه', CetyWebsiteThemeTextDomain); ?></span>
+        </div>
+        <!-- end title of this section -->
+
+        <div class="space-20"></div> <!-- this is for spacing -->
+
+        <!-- all comments -->
+        <section>
+            <div class="comments-list">
+
+                <?php wp_list_comments(); ?>
+
+            </div>
+        </section>
+        <!-- end all comments -->
+
+    </section>
+    <!-- end comments list -->
+<?php
+}
+?>
