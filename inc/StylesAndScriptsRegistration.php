061788c2533150e7f636ba223eb5b1f99bbd9b80
Abbas <abbasbabaei76@gmail.com>
2019-09-22 19:49:11 +0330

    update naming

--- inc/StylesAndScriptsRegistration.php
+++ inc/StylesAndScriptsRegistration.php
@@ -4,11 +4,11 @@
  * keep all styles and scripts registrations in one place
  */
 
-wp_register_style('FontAwesome', get_template_directory_uri() . '/assets/CSS/AllFontAwesomes.css');
-wp_register_style('ColorStyle', get_template_directory_uri() . '/assets/CSS/Color.css');
-wp_register_style('WebsiteStyle', get_template_directory_uri() . '/assets/CSS/Style.css', array('ColorStyle'));
-wp_register_style('CustomFontAwesome', get_template_directory_uri() . '/assets/CSS/CustomFontAwesome.css', array('FontAwesome'));
-wp_register_script('WebsiteJs', get_template_directory_uri() . '/assets/JS/Actions.js');
+wp_register_style('FontAwesome', get_template_directory_uri() . '/Assets/CSS/AllFontAwesomes.css');
+wp_register_style('ColorStyle', get_template_directory_uri() . '/Assets/CSS/Color.css');
+wp_register_style('WebsiteStyle', get_template_directory_uri() . '/Assets/CSS/Style.css', array('ColorStyle'));
+wp_register_style('CustomFontAwesome', get_template_directory_uri() . '/Assets/CSS/CustomFontAwesome.css', array('FontAwesome'));
+wp_register_script('WebsiteJs', get_template_directory_uri() . '/Assets/JS/Actions.js');
 
 //Bootstrap
 wp_register_style('Bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
